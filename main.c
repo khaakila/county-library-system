f4035d6ad9e928fe339a119d3adef65c50845930
khaakila <98585432+khaakila@users.noreply.github.com>
2022-03-04 13:53:59 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,34 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
-int menu();// prototype
+#include<string>
+struct patron
+srting name[100];
+sring pass [30];
+sring email[50];
+int is staff;
+void add patron []
+struct Patron patron;
+printf[enter namespacegets]
+gets (patron.name)
+printf("enter email:");
+gets(patron.pass)
+printf("enter 1 if staff 0 otherwise:");
+scanf("%d",&patron.is_staff);
+printf(patron %succefully added/n")patron name):
+
+
+int menu();//prototype
 void execute_action(int action);
 int main()
 {
     printf("COUNTY LIBRARY SYSTEM!\n");
-    printf("welcome Mr.Titus!\n");
+    printf("welcome margaret!\n");
     execute_action(menu());
     return 0;
 }
- int menu()
- {
-   int action;
-   do {
-      printf( "select an action below\n");
-      printf( "1.Add new patron\n");
-      printf( "2.View patrons\n");
-      printf( "3.View books\n");
-      printf( "4.Add new book\n");
-      printf( "5.Add new book\n");
-      printf("your action(1-4):\n");
-      scanf("%d",&action);
-      if(action<1 || action> 4){
-            system("cls");
-      printf("invalid Action. Try again.\n");
+
+void menu()
+{
+    int action;
+    do{
+        printf("select an action:\n");
+        printf("1. Add new patron\n");
+        printf("2. View patrons\n");
+        printf("3. View books\n");
+        printf("4. Add new book\n");
+        scanf("%d",&action);
+        if(action <1 || action >4){
+                system("cls");
+        printf("invalid action. try again.\n");
+
+        }
+}while (action<1 || action >4);
 }
-    } while(action < 1 || action > 4);
-    return action;
-}
-void execute_action(int action){
- printf("the action is %d",action);
-}
+
+
